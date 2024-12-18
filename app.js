a1eb17abeba111b298770372e277727578999d89
Nicole Ferrer <nicferrer.arq@gmail.com>
2024-12-18 07:09:02 -0300

    alterando limite para 100

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
     //titulo.innerHTML = 'Jogo do número secreto';
 
 let listaDeNumerosSorteados = [];
-let numeroLimite = 10;
+let numeroLimite = 100;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -24,7 +24,7 @@ function exibirTextoNaTela(tag, texto) {
 
 function exibirTextoInicial(){
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
 }
 
 exibirTextoInicial();
